080ec23ddad6c42528d53ae36b0621463a3ee3bc
ThietNN <nguyenthiet1225@gmail.com>
2022-02-10 08:47:41 +0700

    new pj

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter width");
+        double width = sc.nextDouble();
+        System.out.println("Enter height");
+        double height = sc.nextDouble();
+
+        Rectangle rec = new Rectangle(width,height);
+        System.out.println("Your Rectangle: " + rec.display());
+        System.out.println("Perimeter of the Rectangle: " + rec.getPerimeter());
+        System.out.println("Area of the Rectangle: " + rec.getArea());
+    }
+}
